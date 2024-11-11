02dce4a7d30ecc8b95c0d4087de72a068389eeb6
Alvaro Veiga <alvarojoao17@gmail.com>
2024-11-11 20:02:57 -0300

    separating server from application

--- bin/server.js
+++ bin/server.js
@@ -1,25 +1,11 @@
-'use strict'
-
+const app = require('../src/app');
+const debug = require('debug')('balta:server');
 const http = require('http');
-const debug = require('debug')('crudapis:server');
-const express = require('express');
-const { title } = require('process');
-const { version } = require('os');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req,res,next) => {
-    res.status(200).send({
-        title: "Node Store Api",
-        version: "0.0.1"
-    })
-})
-app.use('/',route);
 
 server.listen(port);
 server.on('error', onError);
