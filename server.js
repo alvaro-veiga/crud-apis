1e319332042612f2baa336673d4fe347a4207682
Alvaro Veiga <alvarojoao17@gmail.com>
2024-11-11 19:36:55 -0300

    create normalizePort

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { title } = require('process');
 const { version } = require('os');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -23,3 +23,16 @@ app.use('/',route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0 ) {
+        return port;
+    }
+
+    return false
+}
